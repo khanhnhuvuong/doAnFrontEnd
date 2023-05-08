ff9c794ac7e4353edc16436fe751fbbc3796d9c3
khanhnhuvuong <115868825+khanhnhuvuong@users.noreply.github.com>
2023-05-08 20:43:12 +0700

    first commit template mbd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Register from "./components/register";
+import Login from "./components/login";
+import Chat from "./components/chatApp";
+function App() {
+        return (
+            <div className={App}>
+                <Chat/>
+            </div>
+        );
+}
+export default App;
