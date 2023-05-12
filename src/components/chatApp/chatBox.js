09b11bc38cddf8dd813e13c1a88f49d94ddc774e
khanhnhuvuong <115868825+khanhnhuvuong@users.noreply.github.com>
2023-05-12 11:34:08 +0700

    cap nhat components

---
+++ src/components/chatApp/chatBox.js
@@ -0,0 +1,220 @@
+import React, {useEffect, useState} from "react";
+import './chatApp.css';
+import { useHistory, Link } from 'react-router-dom';
+
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBIcon,
+    MDBBtn,
+    MDBTypography,
+    MDBTextArea,
+    MDBCardHeader, MDBInput,
+} from "mdb-react-ui-kit";
+
+function Chat() {
+    const [socket, setSocket] = useState(null);
+    const history = useHistory();
+
+    const handleLogout = () => {
+        //Gửi yêu cầu đăng ký đến server WebSocket
+        const requestData = {
+            action: "onchat",
+            data: {
+                event: "LOGOUT",
+            },
+        };
+        history.push("/");
+
+        // if (response.ok) {
+        //     const { token } = await response.json();
+        //     localStorage.setItem('token', token);
+        //     // Đặt trạng thái xác thực trong ứng dụng của bạn
+        // } else {
+        //     // Xử lý lỗi đăng nhập
+        // }
+    };
+
+    const ChatList = ({ message }) => {
+
+    }
+
+    return (
+
+                <MDBCol md="6" lg="5" xl="4" className="mb-4 mb-md-0">
+                    <MDBInput wrapperClass='mb-2' label='Nhập tên phòng' size='lg' id='form1' type='text'/>
+                    <MDBBtn className='w-20 m-lg-3 gradient-custom-3' size='lg' onClick={handleLogout}>Thêm</MDBBtn>
+                    <MDBBtn className='w-50 m-lg-3 gradient-custom-3' size='lg' onClick={handleLogout}>Đăng xuất</MDBBtn>
+                    <h5 className="font-weight-bold mb-3 text-center text-lg-start">
+                        Member
+                    </h5>
+
+                    <MDBCard>
+                        <MDBCardBody>
+                            <MDBTypography listUnStyled className="mb-0">
+                                <li
+                                    className="p-2 border-bottom"
+                                    style={{ backgroundColor: "#eee" }}
+                                >
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">John Doe</p>
+                                                <p className="small text-muted">
+                                                    Hello, Are you there?
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">Just now</p>
+                                            <span className="badge bg-danger float-end">1</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2 border-bottom">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Danny Smith</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">5 mins ago</p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2 border-bottom">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Alex Steward</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">Yesterday</p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2 border-bottom">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-3.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Ashley Olsen</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">Yesterday</p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2 border-bottom">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-4.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Kate Moss</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">Yesterday</p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2 border-bottom">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Lara Croft</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">Yesterday</p>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="p-2">
+                                    <a href="#!" className="d-flex justify-content-between">
+                                        <div className="d-flex flex-row">
+                                            <img
+                                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                                                alt="avatar"
+                                                className="rounded-circle d-flex align-self-center me-3 shadow-1-strong"
+                                                width="60"
+                                            />
+                                            <div className="pt-1">
+                                                <p className="fw-bold mb-0">Brad Pitt</p>
+                                                <p className="small text-muted">
+                                                    Lorem ipsum dolor sit.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="pt-1">
+                                            <p className="small text-muted mb-1">5 mins ago</p>
+                                            <span className="text-muted float-end">
+                        <MDBIcon fas icon="check" />
+                      </span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </MDBTypography>
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+
+    );
+}
+
+export default Chat;
