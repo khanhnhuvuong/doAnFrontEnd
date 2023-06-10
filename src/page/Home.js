9d7d56b4e71eb089c52ece366c984d416b61e9f7
khanhnhuvuong <20130357@st.hcmuaf.edu.vn>
2023-06-10 13:30:42 +0700

    chinh css

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {
-    MDBContainer,
-    MDBRow,
-} from "mdb-react-ui-kit";
+import {MDBCol, MDBContainer, MDBRow} from "mdb-react-ui-kit";
 import ChatList from "../components/chatApp/chatList";
 import ChatBox from "../components/chatApp/chatBox";
 import TextArea from "../components/chatApp/textArea";
+import {type} from "@testing-library/user-event/dist/type";
 
 function Home() {
     const [socket, setSocket] = useState(null);
@@ -17,16 +15,17 @@ function Home() {
 
     function handleClickMess(userName, type) {
         setSelectedMess(userName);
-        setSelectedMess(type);
+        setType(type);
         console.log(userName);
-        if (type == 1) {
+
+        if (type === 1) {
             const requestRelogin = {
                 action: "onchat",
                 data: {
                     event: "RE_LOGIN",
                     data: {
-                        user: sessionStorage.getItem('user'),
-                        code: sessionStorage.getItem('relogin_code')
+                        user: sessionStorage.getItem("user"),
+                        code: sessionStorage.getItem("relogin_code"),
                     },
                 },
             };
@@ -43,31 +42,34 @@ function Home() {
                 },
             };
             socket.send(JSON.stringify(requestRoomChatMessage));
-            console.log("Đã gửi yêu cầu lay tin nhan");
+            console.log("Đã gửi yêu cầu lấy tin nhắn");
+
             socket.onmessage = (event) => {
                 const response = JSON.parse(event.data);
-                if (response.status === 'success' && response.event === 'RE_LOGIN') {
-                    console.log("Đã relogin thành công")
+                if (response.status === "success" && response.event === "RE_LOGIN") {
+                    console.log("Đã relogin thành công");
                 } else {
-                    console.log(response.mes)
+                    console.log(response.mes);
                 }
-                if (response.status === 'success' && response.event === 'GET_ROOM_CHAT_MES') {
+                if (
+                    response.status === "success" &&
+                    response.event === "GET_ROOM_CHAT_MES"
+                ) {
                     const chatContent = response.data.chatData;
                     setChatContent(chatContent);
                     console.log(chatContent);
                 } else {
-                    console.log(response.mes)
+                    console.log(response.mes);
                 }
-            }
+            };
         } else {
-
             const requestRelogin = {
                 action: "onchat",
                 data: {
                     event: "RE_LOGIN",
                     data: {
-                        user: sessionStorage.getItem('user'),
-                        code: sessionStorage.getItem('relogin_code')
+                        user: sessionStorage.getItem("user"),
+                        code: sessionStorage.getItem("relogin_code"),
                     },
                 },
             };
@@ -84,28 +86,74 @@ function Home() {
                 },
             };
             socket.send(JSON.stringify(requestRoomChatMessage));
-            console.log("Đã gửi yêu cầu lay tin nhan");
+            console.log("Đã gửi yêu cầu lấy tin nhắn");
             socket.onmessage = (event) => {
                 const response = JSON.parse(event.data);
-                if (response.status === 'success' && response.event === 'RE_LOGIN') {
-                    console.log("Đã relogin thành công")
+                if (response.status === "success" && response.event === "RE_LOGIN") {
+                    console.log("Đã relogin thành công");
                 } else {
-                    console.log(response.mes)
+                    console.log(response.mes);
                 }
-                if (response.status === 'success' && response.event === 'GET_PEOPLE_CHAT_MES') {
+                if (
+                    response.status === "success" &&
+                    response.event === "GET_PEOPLE_CHAT_MES"
+                ) {
                     const chatContent = response.data;
                     setChatContent(chatContent);
                     console.log(chatContent);
                 } else {
-                    console.log(response.mes)
+                    console.log(response.mes);
                 }
-            }
+            };
         }
-
     }
 
 
 
+    function handleSendMessage(mes) {
+
+        if (selectedMess) {
+            if(typeUser === 1){
+                const requestSendMessageRoom = {
+                    action: "onchat",
+                    data: {
+                        event: "SEND_CHAT",
+                        data: {
+                            type: "room",
+                            to: selectedMess,
+                            mes: mes,
+                        },
+                    },
+                };
+                socket.send(JSON.stringify(requestSendMessageRoom));
+                console.log("gui tin nhan tu",sessionStorage.getItem('user'))
+                console.log("gui tin nhan den",selectedMess)
+                console.log("gui tin nhan voi noi dung",mes)
+            }
+            else{
+                const requestSendMessagePeople = {
+                    action: "onchat",
+                    data: {
+                        event: "SEND_CHAT",
+                        data: {
+                            type: "people",
+                            to: selectedMess,
+                            mes: mes,
+                        },
+                    },
+                };
+                socket.send(JSON.stringify(requestSendMessagePeople));
+                console.log("gui tin nhan tu",sessionStorage.getItem('user'))
+                console.log("gui tin nhan den",selectedMess)
+                console.log("gui tin nhan voi noi dung",mes)
+            }
+
+
+
+
+
+        }
+    }
 
     useEffect(() => {
         // Khởi tạo kết nối với server qua websocket
@@ -113,23 +161,31 @@ function Home() {
 
         socket.addEventListener("open", () => {
             console.log("WebSocket connection established.");
-            const username = sessionStorage.getItem('user');
-            const code = sessionStorage.getItem('code');
+            const username = sessionStorage.getItem("user");
+            const code = sessionStorage.getItem("code");
 
             // Gửi message RE_LOGIN để đăng nhập lại với thông tin user và code
-            socket.send(JSON.stringify({
+            socket.send(
+                JSON.stringify({
                     action: "onchat",
                     data: {
                         event: "RE_LOGIN",
                         data: {
                             user: username,
                             code: code,
-                        }
-                    }
-                }
-            ));
+                        },
+                    },
+                })
+            );
+            // socket.send(JSON.stringify({
+            //         action: "onchat",
+            //         data: {
+            //             event: "GET_USER_LIST",
+            //         }
+            //     }
+            // ));
+            // setSocket(socket);
 
-            setSocket(socket);
         });
 
         // Đóng kết nối khi component unmount
@@ -140,11 +196,15 @@ function Home() {
 
 
     return (
-        <MDBContainer fluid className="py-5 gradient-custom" style={{backgroundColor: "#eee"}}>
+        <MDBContainer fluid className="py-2 gradient-custom" style={{ backgroundColor: "#eee" }}>
             <MDBRow>
-                <ChatList userList={userList} handleClickMess={handleClickMess}/>
-                <ChatBox selectedMess={selectedMess} chatContent={chatContent}/>
-                {/*<TextArea/>*/}
+                <ChatList userList={userList} handleClickMess={handleClickMess} />
+                {selectedMess && (
+                        <MDBCol md="6" lg="7" xl="8">
+                        <ChatBox selectedMess={selectedMess} chatContent={chatContent} />
+                        <TextArea handleSendMessageClick={handleSendMessage} selectedMess={selectedMess} />
+                        </MDBCol>
+                )}
             </MDBRow>
         </MDBContainer>
     );
